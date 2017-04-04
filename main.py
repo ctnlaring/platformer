fc3b0c5c3b0dee6fe12b1ac606ab5b5753bb47ee
Collin Norwood <collinthegeek@gmail.com>
2017-04-04 06:41:29 -0500

    cleaned out unused code

--- main.py
+++ main.py
@@ -18,8 +18,6 @@ jumping=0
 jumptime=0
 ADDENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(ADDENEMY, 3550)
-ADDBIGENEMY = pygame.USEREVENT + 2
-pygame.time.set_timer(ADDBIGENEMY, 2550)
 font = pygame.font.SysFont("monospace", 55)
 pygame.display.set_caption('Platformer Test')
 explosion=pygame.image.load("img/block.png")
@@ -44,7 +42,7 @@ class Player(pygame.sprite.Sprite):
         self.change_x += x
         self.change_y += y
 
-    def update(self, pressed_keys):
+    def update(self):
         if self.rect.right < 0:
             self.kill()
         if self.rect.left > 768:
@@ -70,41 +68,6 @@ class Player(pygame.sprite.Sprite):
             else:
                 self.rect.top = block.rect.bottom
 
-        """for block in blocks:
-                if self.rect.bottom != block.rect.top:
-                    self.change_y=1"""
-
-
-
-
-        """
-        The old movement system. keeping around just in case
-        global jumping
-        global jumptime
-        if pressed_keys[K_SPACE]:
-            if jumping == 0:
-                jumptime = 6
-                jumping = 1
-        if jumptime > 0:
-            jumptime = jumptime-1
-            self.rect.move_ip(0,-10)
-        if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-6, 0)
-        if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(6, 0)
-        if self.rect.bottom < 550:
-            self.rect.move_ip(0,2)
-        if self.rect.bottom == 550:
-            jumping = 0
-        if self.rect.left < 0:
-            self.rect.left = 0
-        elif self.rect.right > 768:
-            self.rect.right = 768
-        if self.rect.top <= 0:
-            self.rect.top = 0
-"""
-
-
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super(Enemy, self).__init__()
@@ -125,48 +88,6 @@ class Enemy(pygame.sprite.Sprite):
         global health
         global deaths
         self.rect.move_ip(-2,0)
-
-class BigEnemy(pygame.sprite.Sprite):
-    def __init__(self):
-        super(BigEnemy, self).__init__()
-        self.image = pygame.image.load('img/enemy2.png').convert()
-        self.image.set_colorkey((255, 255, 255), RLEACCEL)
-        self.rect = self.image.get_rect(
-            center=(random.randint(0,550-32),0))
-    def update(self):
-        if self.rect.right < 0:
-            self.kill()
-        if self.rect.left > 768:
-            self.kill()
-        if self.rect.top > 720:
-            self.kill()
-        if self.rect.bottom < 0:
-            self.kill()
-
-        global health
-        global deaths
-        global enbx
-        global enby
-        self.rect.move_ip(0, 1)
-        if self.rect.bottom > 500:
-            self.kill()
-            explosionsnd.play()
-            for entity in enemies:
-                screen.blit(entity.image, entity.rect)
-            screen.blit(explosion, (player.rect.x-4,player.rect.y))
-            pygame.display.flip()
-            pygame.time.delay(150)
-            health-=1
-            deaths+=1
-        bshoot = random.randint(0,50)
-        if bshoot == 1:
-            enbx = self.rect.x
-            enby = self.rect.y
-            laser.play()
-            new_enbullet = EnBullet()
-            enbullets.add(new_enbullet)
-            all_sprites.add(new_enbullet)
-
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self):
@@ -268,12 +189,6 @@ while True:
             enemies.add(new_enemy)
             all_sprites.add(new_enemy)
 
-        #elif event.type == ADDBIGENEMY:
-            #new_bigenemy = BigEnemy()
-            #enemies.add(new_bigenemy)
-            #all_sprites.add(new_bigenemy)
-
-    healthlabel = font.render(str(health), 1, (255,255,255))
     screen.blit(background, (0, 0))
     exps=-32
     while exps < 700:
@@ -282,9 +197,7 @@ while True:
         blocks.add(block)
         all_sprites.add(block)
 
-    screen.blit(healthlabel, (500, 10))
-    pressed_keys = pygame.key.get_pressed()
-    player.update(pressed_keys)
+    player.update()
     enemies.update()
     blocks.update()
     bullets.update()
@@ -295,7 +208,7 @@ while True:
         pass
     if pygame.sprite.groupcollide(bullets, enemies, True, True, collided = None):
         kills+=1
-    if pygame.sprite.groupcollide(enbullets, players, True, True, collided = None):
+    if pygame.sprite.groupcollide(enemies, players, True, True, collided = None):
         health=0
     if health==0:
         if shots != 0 & deaths != 0:
